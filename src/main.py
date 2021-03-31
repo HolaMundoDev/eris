31365afdc25180459e00136f9d67f58c89481f55
Jheyson Saavedra <jheysonsaav@gmail.com>
2021-03-31 11:42:44 -0500

    feat: create basic bot client

--- src/main.py
+++ src/main.py
@@ -1,1 +1,28 @@
-print("Hello, world!")
+from os import getenv
+from discord import Client
+from dotenv import load_dotenv
+
+
+# Load enviroments from .env file
+load_dotenv()
+
+# Create bot client
+client = Client()
+
+
+@client.event
+async def on_ready():
+    print(f"We have logged in as {client.user}")
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith("!hello"):
+        await message.channel.send("Hello!")
+
+
+if __name__ == "__main__":
+    client.run(getenv("BOT_TOKEN"))
