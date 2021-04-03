a2d3a26e4c9e4a8a743941ae324e9a7d4f8704c1
TeoDev1611 <malehurtadoreyes@hotmail.com>
2021-04-03 11:50:44 -0500

    feat(Add-the-cita-and-joke-command): create this commands

--- src/cogs/help.py
+++ src/cogs/help.py
@@ -16,6 +16,8 @@ class help_command(commands.Cog):
         Help: Muestra este mensaje
         Ban: Banea al usuario si tiene los permisos
         Kick: Kickea al usuario si tiene los permisos
+        Cita: Muestra una cita inspiradora de varios filósofos
+        Joke: Muestra chistes que te divertirán
         
         Chao Mundo
         """
