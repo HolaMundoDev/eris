3f135d49435ca2a7c4ef23c5ea554801dd3a5854
TeoDev1611 <malehurtadoreyes@hotmail.com>
2021-04-23 09:36:26 -0500

    Run the linter and the format

--- src/cogs/pypi.py
+++ src/cogs/pypi.py
@@ -4,19 +4,19 @@ from discord.ext import commands
 
 
 class PypiCommmand(commands.Cog):
-    def __init__(self,bot):
+    def __init__(self, bot):
         self.bot = bot
 
     @commands.command(name="pypi")
-    async def pypi(self,bot,ctx,*,args):
+    async def pypi(self, bot, ctx, *, args):
         try:
             req = requests.get("https://pypi.org/pypi/{args}/json")
             if req.status_code == 200:
                 resp = req.json()
                 info = resp["info"]
                 embed = discord.Embed(
-                    title = f"La información sobre el paquete {args} es:",
-                    description = f"""
+                    title=f"La información sobre el paquete {args} es:",
+                    description=f"""
                     Nombre: {info["name"]}
                     Resumen: {info["summary"]}
                     Autor: {info["author"]}
@@ -26,18 +26,22 @@ class PypiCommmand(commands.Cog):
                     HomePage: {info["home_page"]}
                     Versión de python: {info["requires_python"]}
                     """,
-                    color = discord.Color.random()
+                    color=discord.Color.random(),
                 )
-                embed.set_thumbnail(url="https://media1.tenor.com/images/ac9cf136a3c0f857e436c32561e9b6e8/tenor.gif")
+                embed.set_thumbnail(
+                    url="https://media1.tenor.com/images/ac9cf136a3c0f857e436c32561e9b6e8/tenor.gif"
+                )
                 await ctx.send(embed=embed)
             else:
                 embed = discord.Embed(
                     title="Error :x:",
-                    description ="Error en encontrar el paquete o la API",
-                    footer = f"API STATUS: {req.status_code}",
-                    color = discord.Color.random(),
+                    description="Error en encontrar el paquete o la API",
+                    footer=f"API STATUS: {req.status_code}",
+                    color=discord.Color.random(),
                 )
-                embed.set_thumbnail(url="https://media1.tenor.com/images/46ce1235c5697ce170c6e84f4b4fb4e7/tenor.gif")
+                embed.set_thumbnail(
+                    url="https://media1.tenor.com/images/46ce1235c5697ce170c6e84f4b4fb4e7/tenor.gif"
+                )
         except Exception as err:
             embed = discord.Embed(
                 title="Error API STATUS",
@@ -48,5 +52,7 @@ class PypiCommmand(commands.Cog):
                 url="https://media1.tenor.com/images/46ce1235c5697ce170c6e84f4b4fb4e7/tenor.gif"
             )
             await ctx.send(embed=embed)
+
+
 def setup(bot):
     bot.add_cog(PypiCommmand(bot))
